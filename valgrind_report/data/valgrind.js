b033212f7eb9a7882bc73fd7acdb2892e7aacee6
Bertrand Coconnier <bcoconni@users.sf.net>
2020-04-24 20:12:36 +0200

    Moved the navigation buttons to a better location.

--- valgrind_report/data/valgrind.js
+++ valgrind_report/data/valgrind.js
@@ -3,6 +3,7 @@ $(document).ready(
         $("div.issue").accordion({ collapsible: true, active: false });
         $("button.next_error").button();
         $("button.next_error").click(function () {
+            event.stopPropagation();
             var index = $(this).parents("div.issue").index("div.issue");
             $([document.documentElement, document.body]).animate({
                 scrollTop: $("div.issue").eq(index + 1).offset().top
@@ -11,6 +12,7 @@ $(document).ready(
         $("button.next_error:last").hide();
         $("button.prev_error").button();
         $("button.prev_error").click(function () {
+            event.stopPropagation();
             var index = $(this).parents("div.issue").index("div.issue");
             $([document.documentElement, document.body]).animate({
                 scrollTop: $("div.issue").eq(index - 1).offset().top
