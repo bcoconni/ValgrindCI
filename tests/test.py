c907389ace7ed99a176fb19468087cf5f7bcea72
Bertrand Coconnier <bcoconni@users.sf.net>
2020-09-27 14:37:35 +0200

    Added a test of ValgrindData.list_error_kinds()

--- tests/test.py
+++ tests/test.py
@@ -4,14 +4,21 @@ import unittest
 from ValgrindCI import ValgrindData
 
 
-class TestNumberOfErrors(unittest.TestCase):
+class TestValgrindCI(unittest.TestCase):
     def test_number_of_errors(self):
         data = ValgrindData()
         data.parse("test.xml")
         self.assertEqual(data.get_num_errors(), 2)
 
+    def test_list_error_kinds(self):
+        data = ValgrindData()
+        data.parse("test.xml")
+        self.assertEqual(
+            data.list_error_kinds(), ["UninitCondition", "Leak_DefinitelyLost"]
+        )
 
-suite = unittest.TestLoader().loadTestsFromTestCase(TestNumberOfErrors)
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestValgrindCI)
 test_result = unittest.TextTestRunner(verbosity=2).run(suite)
 if test_result.failures or test_result.errors:
     sys.exit(-1)
